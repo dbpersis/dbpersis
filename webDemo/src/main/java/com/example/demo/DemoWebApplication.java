3d63670c95d49b571a7053718b421fbcd772cf29
wwf <123472679@qq.com>
2017-10-28 19:38:04 +0800

    格式化代码

--- webDemo/src/main/java/com/example/demo/DemoWebApplication.java
+++ webDemo/src/main/java/com/example/demo/DemoWebApplication.java
@@ -9,14 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@EnableTransactionManagement
 public class DemoWebApplication {
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		SpringApplication.run(DemoWebApplication.class, args);
-	}
-	
-=======
   public static void main(String[] args) {
     SpringApplication.run(DemoWebApplication.class, args);
   }
->>>>>>> 0f87353f6acdae07abb0d26d7c0a765bc45ed806
+
 }
--- webDemo/src/main/java/com/example/demo/controller/DemoController.java
+++ webDemo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,37 +1,36 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.User;
+import com.terry.starter.DbsersisService;
 import java.sql.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.bean.User;
-import com.terry.starter.DbsersisService;
-
 @RestController
-@RequestMapping(value="/demo")
+@RequestMapping(value = "/demo")
 public class DemoController {
-	@Autowired
-    private DbsersisService exampleService;
 
-    @RequestMapping(value="input",method=RequestMethod.GET)
-    public String input(String word){
-    	
-		try {
-			User sc = new User();
-			sc.setId("terry");
-			sc.setAge(19);
-			sc.setUsername("User");
-			sc.setPwd("21232f297a57a5a743894a0e4a801fc3");
-			sc.setCreatedate(new Date(System.currentTimeMillis()));
-			exampleService.save(sc);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        return word;
+  @Autowired
+  private DbsersisService exampleService;
+
+  @RequestMapping(value = "input", method = RequestMethod.GET)
+  public String input(String word) {
+
+    try {
+      User sc = new User();
+      sc.setId("terry");
+      sc.setAge(19);
+      sc.setUsername("User");
+      sc.setPwd("21232f297a57a5a743894a0e4a801fc3");
+      sc.setCreatedate(new Date(System.currentTimeMillis()));
+      exampleService.save(sc);
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
+    return word;
+  }
 
 }
